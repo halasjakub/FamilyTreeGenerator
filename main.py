26b816ce975a3dc16b23b5edd8f99b6c2db9a811
halasjakub <83594926+halasjakub@users.noreply.github.com>
2025-02-20 17:02:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from tkinter import Menu
+from tkinter import simpledialog
 
-
+def open_new_window():
+    db_name = simpledialog.askstring("Database Name", "Enter the name of the new database:")
+    if db_name:
+        print(f"New database name: {db_name}")
 
 window = tk.Tk()
 window.title("Family Tree Generator")
@@ -14,7 +18,7 @@ window.config(menu=menu_bar)
 # File options
 file_menu = Menu(menu_bar, tearoff=False)
 menu_bar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New")
+file_menu.add_command(label="New", command=open_new_window)
 file_menu.add_command(label="Open")
 file_menu.add_command(label="Close")
 
@@ -23,6 +27,5 @@ add_menu = Menu(menu_bar, tearoff=False)
 menu_bar.add_cascade(label="Add", menu=add_menu)
 add_menu.add_command(label="Person")
 add_menu.add_command(label="Relation")
-
 
 window.mainloop()
